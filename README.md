4218359aabef3a9757631452bcd15e418e410b56
tankiso <sebengamt@gmail.com>
2019-02-25 13:12:02 +0200

    product controller

---
+++ README.md
@@ -0,0 +1,19 @@
+# Realmdigital Pre-Interview Refactoring #
+
+The code contained herein contains duplication, inconsistencies, implements bad practices and is untestable.
+
+### What to do? ###
+
+* Fork this repository
+* Refactor the code to be more maintainable and testable, using best practices
+* If you are interviewing for a C# position, work with/reference the C# folder, otherwise reference the PHP folder
+* Send a link to your forked repository, and your CV to jobs@realmdigital.co.za
+
+### Extra Notes ###
+
+* Please note the endpoint - http://192.168.0.241/eanlist?type=Web - is not a real endpoint (there is a representation of what this data might look like in the `mock-data` folder
+
+### Contribution guidelines ###
+
+* Never clone this repository unless the intention is to improve or update it with changes that will be used in all future projects
+* By forking the repository to your new project, any improvements made to this repo can be integrated into your project using git pull mechanism
---
+++ php/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Realmdigital\Web\Controller;
+
+use DDesrosiers\SilexAnnotations\Annotations as SLX;
+use Silex\Application;
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * @SLX\Controller(prefix="product/")
+ */
+class ProductController {
+
+    /**
+     * @SLX\Route(
+     *      @SLX\Request(method="GET", uri="/{id}")
+     * )
+     * @param Application $app
+     * @param $name
+     * @return
+     */
+    public function getById_GET(Application $app, $id){
+       
+        $requestData = array();
+        $requestData['id'] = $id;
+        $body = $requestData['id'];
+        $request = new Request('HEAD', 'http://192.168.0.241/eanlist?type=Web', $body);
+        $response = $client->sendAsync($request);
+        $response = json_decode($response);
+
+        $result = [];
+        foreach($response as $product {
+            $prod = array();
+            $prod['ean']=$product->barcode;
+            $$prod["name"]=$product->itemName;
+            $prod["prices"] = array();
+            $prodPriceRecords =$product->priceRecords;
+
+            foreach($prodPriceRecords as $price) {
+                if ($price->CurrencyCode != 'ZAR') {
+                    $p_price = array();
+                    $p_price['price']  = $price->sellingPrice;
+                    $p_price['currency'] = $price->currencyCode;
+                    $prod["prices"][] = $p_price;
+                }
+            }
+            $result[] = $prod;
+        }
+
+        return $app->render('products/product.detail.twig', $result);
+    }
+
+    /**
+     * @SLX\Route(
+     *      @SLX\Request(method="GET", uri="/search/{name}")
+     * )
+     * @param Application $app
+     * @param $name
+     * @return
+     */
+    public function getByName_GET(Application $app, $name){
+      
+        $requestData = array();
+        $requestData['names'] = $name;
+        $body = $requestData['id'];
+        $request = new Request('HEAD', 'http://192.168.0.241/eanlist?type=Web', $body);
+        $response = $client->sendAsync($request);
+        $response = json_decode($response);
+
+        $result = [];
+        foreach($response as $product {
+            $prod = array();
+            $prod['ean']=$product->barcode;
+            $$prod["name"]=$product->itemName;
+            $prod["prices"] = array();
+            $prodPriceRecords =$product->priceRecords;
+
+            foreach($prodPriceRecords as $price) {
+                if ($price->CurrencyCode != 'ZAR') {
+                    $p_price = array();
+                    $p_price['price']  = $price->sellingPrice;
+                    $p_price['currency'] = $price->currencyCode;
+                    $prod["prices"][] = $p_price;
+                }
+            }
+            $result[] = $prod;
+        }
+
+        return $app->render('products/products.twig', $result);
+    }
+
+}
